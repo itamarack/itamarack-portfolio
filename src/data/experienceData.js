49673bf13da7daea41775361911f2cbadb553517
itamarack <tamaraabells@gmail.com>
2024-02-17 22:58:48 +0000

    updated

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Safeguarding Network',
+        jobtitle: 'Laravel Developer',
+        startYear: 'January 2024',
+        endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Cloudenly HR',
+        jobtitle: 'PHP - JavaScript Developer',
+        startYear: 'May 2022',
+        endYear: 'January 2024'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Rapid Capsule - A Telemedicine Company',
+        jobtitle: 'FullStack Developer',
+        startYear: 'February 2021',
+        endYear: 'May 2022'
     },
 ]
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,9 +2,9 @@ import resume from '../assets/pdf/resume.pdf'
 import headerImage from '../assets/images/web-dev-programmer.jpg'
 
 export const headerData = {
-    name: 'Jane Doe',
+    name: 'Tamara Innomesanghan',
     title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"There wasn't a bird in the sky, but that was not what caught his attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. He knew what was coming and he hoped he was prepared. ",
     image: headerImage,
     resumePdf: resume
 }
