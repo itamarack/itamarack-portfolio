d78a10c7bdce8708b59f65dda4db9f8d2606ce41
itamarack <tamaraabells@gmail.com>
2024-02-17 22:40:21 +0000

    converted to tailwindcss starts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
-
+import "./index.css"
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
